47f90bd14b2c7a17f2f8a5632f1b3a566269e69f
Wasanta <kwan_u_day@hotmail.com>
2021-11-28 13:38:55 -0500

    Revert "added"
    This reverts commit 8a295575f418b7364350bc42ff62bdca07939100.

--- src/Parent.js
+++ src/Parent.js
@@ -5,18 +5,11 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-  const [childrenColor, setChildrenColor] = useState("#FFF")
-
-  function handleChangeColor(newChildColor) {
-    const newRandomColor = getRandomColor();
-    setColor(newRandomColor);
-    setChildrenColor(newChildColor);
-  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child color={childrenColor} onChangeColor={handleChangeColor} />
-      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child />
+      <Child />
     </div>
   );
 }
